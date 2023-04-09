0c84f6c7589cf7371eb63dac1e517f156c71cbac
Quan Qinle <quanqinle@gmail.com>
2023-04-09 23:13:35 +0800

    Support to covert multiple books in one txt file. Haven't finished yet!

---
+++ src/main/java/com/quanqinle/epub/entity/FileInfoOld.java
@@ -0,0 +1,85 @@
+package com.quanqinle.epub.entity;
+
+import java.nio.file.Path;
+
+/**
+ * File info, such as .xhtml
+ *
+ * @author quanqinle
+ */
+public class FileInfoOld {
+  /** the index in the same type files */
+  int order;
+  /** file name with suffix */
+  String name;
+  /** the suffix of file name, including . sign */
+  String suffix;
+  /** file name without suffix */
+  String shortName;
+  /**
+   * such as `<title>` or `<h1>` in HTML
+   */
+  String describe;
+  /** the full path */
+  Path fullPath;
+
+  public FileInfoOld() {}
+
+  public FileInfoOld(String name) {
+    this.name = name;
+  }
+
+  public FileInfoOld(int order, String name, String describe) {
+    this.order = order;
+    this.name = name;
+    this.describe = describe;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+
+  public void setOrder(int order) {
+    this.order = order;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSuffix() {
+    return suffix;
+  }
+
+  public void setSuffix(String suffix) {
+    this.suffix = suffix;
+  }
+
+  public String getShortName() {
+    return shortName;
+  }
+
+  public void setShortName(String shortName) {
+    this.shortName = shortName;
+  }
+
+  public String getDescribe() {
+    return describe;
+  }
+
+  public void setDescribe(String describe) {
+    this.describe = describe;
+  }
+
+  public Path getFullPath() {
+    return fullPath;
+  }
+
+  public void setFullPath(Path fullPath) {
+    this.fullPath = fullPath;
+  }
+}
