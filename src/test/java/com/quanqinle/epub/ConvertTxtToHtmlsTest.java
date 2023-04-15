a7d5c71caa5e47c7d77e498eeff8f3d44672e859
Quan Qinle <quanqinle@gmail.com>
2023-04-15 22:51:09 +0800

    support to read book info from yaml file

--- src/test/java/com/quanqinle/epub/ConvertTxtToHtmlsTest.java
+++ src/test/java/com/quanqinle/epub/ConvertTxtToHtmlsTest.java
@@ -3,7 +3,6 @@ package com.quanqinle.epub;
 import com.quanqinle.epub.entity.BookInfo;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
@@ -13,30 +12,27 @@ class ConvertTxtToHtmlsTest {
 
   @Test
   void convert() {
-    // 源文件
-    Path srcFilePath =
-            Paths.get("C:\\Users\\quanql\\Desktop\\《亵渎》烟雨江南.txt");
-
-    // 目标文件
     BookInfo book = new BookInfo();
-    book.setOutputDir(Paths.get("c:","MyData", "epub"));
+    // 源文件
+    book.setSrcTxtPath(Paths.get("D:", "红楼梦.txt"));
+    // 目标文件
+    book.setOutputDir(Paths.get("D:","demo"));
     book.setHasManyBooks(true);
 
     /* 以下属性非必须 */
-    book.setBookTitle("《亵渎》");
-    book.setAuthor("烟雨江南");
+    book.setBookTitle("红楼梦");
+    book.setAuthor("曹雪芹");
     // 设置 jpg 格式的封面图
-//    book.setCoverJpgFullPath(Paths.get("D:", "book.jpg"));
+    book.setCoverJpgFullPath(Paths.get("D:", "book.jpg"));
 
     // 建议设置上面3个
 
-    book.setUUID(UUID.randomUUID().toString());
+    book.setUuid(UUID.randomUUID().toString());
     book.setLanguage("zh");
     book.setCreateDate("2023-04-07");
-//    book.setIsbn("xyz");
 
-    ConvertTxtToHtmls parse = new ConvertTxtToHtmls(srcFilePath, book);
+    ConvertTxtToHtmls parse = new ConvertTxtToHtmls(book);
     parse.convert();
-
   }
+
 }
--- src/test/java/com/quanqinle/epub/MakeEpubFromTemplateTest.java
+++ src/test/java/com/quanqinle/epub/MakeEpubFromTemplateTest.java
@@ -1,10 +1,13 @@
 package com.quanqinle.epub;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.quanqinle.epub.entity.BookInfo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -21,30 +24,44 @@ class MakeEpubFromTemplateTest {
   void tearDown() {}
 
   @Test
-  void make() {
-    // 源文件
-    Path srcFilePath =
-        Paths.get("C:\\Users\\quanql\\Desktop\\《亵渎》烟雨江南.txt");
+  void makeByBookInfoInYaml() throws IOException {
 
-    // 目标文件
+    // 书的配置文件
+    ObjectMapper mapper = new YAMLMapper();
+    BookInfo book = mapper.readValue(Files.newInputStream(Path.of("bookinfo.yaml")), BookInfo.class);
+
+    // make srcFilePath to a .epub book
+    MakeEpubFromTemplate makeEpub = new MakeEpubFromTemplate(book);
+    try {
+      makeEpub.make();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Test
+  void make() {
+
     BookInfo book = new BookInfo();
-    book.setOutputDir(Paths.get("c:","MyData", "epub"));
+    // 源文件
+    book.setSrcTxtPath(Paths.get("D:", "红楼梦.txt"));
+    // 目标文件
+    book.setOutputDir(Paths.get("D:","demo"));
     book.setHasManyBooks(true);
 
     /* 以下属性非必须 */
-    book.setBookTitle("《亵渎》");
-    book.setAuthor("烟雨江南");
+    book.setBookTitle("红楼梦");
+    book.setAuthor("曹雪芹");
     // 设置 jpg 格式的封面图
-//    book.setCoverJpgFullPath(Paths.get("D:", "book.jpg"));
+    book.setCoverJpgFullPath(Paths.get("D:", "book.jpg"));
 
     // 建议设置上面3个
 
-    book.setUUID(UUID.randomUUID().toString());
+    book.setUuid(UUID.randomUUID().toString());
     book.setLanguage("zh");
     book.setCreateDate("2023-04-07");
 
     // make srcFilePath to a .epub book
-    MakeEpubFromTemplate makeEpub = new MakeEpubFromTemplate(srcFilePath, book);
+    MakeEpubFromTemplate makeEpub = new MakeEpubFromTemplate(book);
     try {
       makeEpub.make();
     } catch (IOException e) {
